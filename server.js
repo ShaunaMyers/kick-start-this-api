a593b1733f6585ec1e37ce678681f3c4d3190f57
Shauna Myers <shaunamyers26@gmail.com>
2022-01-08 18:40:49 -0700

    Require cors & use app.use with cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(5000, () => {
     console.log('Server has started on port 5000')
