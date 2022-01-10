1fb62a150e8b60fce8dae65da83dfb3416e6328b
Shauna Myers <shaunamyers26@gmail.com>
2022-01-09 18:40:08 -0700

    Update funds_raised reqParam in Post

--- server.js
+++ server.js
@@ -8,22 +8,31 @@ app.use(express.json());
 
 // Routes
 
+app.get('/', (req, res) => {
+    pool.query('SELECT * FROM products', (err, response) => {
+      console.log(err, response)
+      err
+      ? res.status(500).send('Database Error')
+      : res.status(200).send({rows: response.rows})
+    })
+})
+
 // Create a product
 
 app.post('/products', (req, res) => {
-    const reqParams = ['title', 'description', 'funds_goal', 'funds_raised', 'images', 'creator_name', 'creator_email'];
+    const reqParams = ['title', 'description', 'funds_goal', 'images', 'creator_name', 'creator_email'];
     let error = false;
     reqParams.forEach((param, index) => {
 
       if (!req.body[param]) {
         res.status(422).send('Please send all required data');
         error = true;
-      } else if (index === 6 && !error) {
+      } else if (index === 5 && !error) {
         const { title, description, funds_goal, funds_raised, images, creator_name, creator_email } = req.body;
 
-        pool.query(`INSERT INTO products (title, description, funds_goal, funds_raised, images, creator_name, creator_email) VALUES ('${title}', '${description}', ${funds_goal}, ${funds_raised}, ${images}, '${creator_name}', '${creator_email}')`,
+        pool.query(`INSERT INTO products (title, description, funds_goal, funds_raised, images, creator_name, creator_email) VALUES ('${title}', '${description}', ${funds_goal}, ${funds_raised}, '${images}', '${creator_name}', '${creator_email}')`,
         (err, response) => {
-          console.log(err, response)
+          console.log('this is error', err, response)
           err 
           ? res.status(500).send('Database Error')
           : res.status(200).send({ title, description, funds_goal, funds_raised, images, creator_name, creator_email })
