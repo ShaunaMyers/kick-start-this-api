4cf12403e42dec680ec63cf1262d48810432468e
Shauna Myers <shaunamyers26@gmail.com>
2022-01-09 17:42:13 -0700

    Write post method

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.send('Express application working ...');
-});
+// Routes
+
+// Create a product
+app.post("/products", async (req,res) => {
+    try {
+        console.log(req.body)
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+app.post('/products', (req, res) => {
+    const reqParams = ['title', 'description', 'funds_goal', 'funds_raised', 'images', 'creator_name', 'creator_email'];
+    let error = false;
+    reqParams.forEach((param, index) => {
+
+      if (!req.body[param]) {
+        res.status(422).send('Please send all required data');
+        error = true;
+      } else if (index === 6 && !error) {
+        const { title, description, funds_goal, funds_raised, images, creator_name, creator_email } = req.body;
+
+        pool.query(`INSERT INTO products (title, description, funds_goal, funds_raised, images, creator_name, creator_email) VALUES ('${title}', '${description}', ${funds_goal}, ${funds_raised}, ${images}, '${creator_name}', '${creator_email}')`,
+        (err, response) => {
+          console.log(err, response)
+          err 
+          ? res.status(500).send('Database Error')
+          : res.status(200).send({ title, description, funds_goal, funds_raised, images, creator_name, creator_email })
+        })
+      }
+    })
+  })
+
+// Get all products
+
+
+// Update funding on project
+
+// Delete product
 
 app.listen(3000, () => {
     console.log('Server has started on port 3000')
