5e578e521ffbdf1c4f9260e966222eb04f79cdb9
Shauna Myers <shaunamyers26@gmail.com>
2022-01-11 23:15:36 -0700

    Change config SSL

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 
 const { Pool } = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: { rejectUnauthorized: false }
-})
+const pool = (() => {
+    if (process.env.NODE_ENV !== 'production') {
+        return new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: false
+        });
+    } else {
+        return new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+              }
+        });
+    } })();
 
 module.exports = { pool }
