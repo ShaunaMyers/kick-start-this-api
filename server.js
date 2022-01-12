75b3cbc5ec30a0298290e63fcc0661862e3afb8b
Shauna Myers <shaunamyers26@gmail.com>
2022-01-11 20:22:15 -0700

    Revert to older version of server.js

--- server.js
+++ server.js
@@ -1,72 +1,68 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
-const { response } = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 80;
+const pool = require("./db");
+app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
 app.use(express.json());
-const queries = require('./models/dbHelpers');
-// app.use(cors());
 
-app.use(cors({
-  origin: [
-    'https://kickstartthisapi.herokuapp.com/',
-    'http://localhost:80',
-    'http://localhost:3000'
-  ],
-}));
-
-app.get('/products', (req, res) => {
-  queries.getAllProducts()
-  .then(product => {
-    res.status(200).send(product)
-  })
-  .catch(error => {
-    res.status(500).send(error.message)
-  })
-});
+app.get('/', (req, res) => {
+    pool.query('SELECT * FROM products', (err, response) => {
+      console.log(err, response)
+      err
+      ? res.status(500).send('Database Error')
+      : res.status(200).send({rows: response.rows})
+    })
+})
 
 app.post('/products', (req, res) => {
-  queries.addProduct(req.body)
-  .then(product => {
-    res.status(200).send(product)
-  })
-  .catch(error => {
-    res.status(422).send({ message: 'Bad request' })
-    res.status(500).send({ message: 'Database error creating a product' })
-  })
-});
+    const reqParams = ['title', 'description', 'funds_goal', 'images', 'creator_name', 'creator_email'];
+    let error = false;
+    reqParams.forEach((param, index) => {
+
+      if (!req.body[param]) {
+        res.status(422).send('Please send all required data');
+        error = true;
+      } else if (index === 5 && !error) {
+        const { title, description, funds_goal, funds_raised, images, creator_name, creator_email } = req.body;
+
+        pool.query(`INSERT INTO products (title, description, funds_goal, funds_raised, images, creator_name, creator_email) VALUES ('${title}', '${description}', ${funds_goal}, ${funds_raised}, '${images}', '${creator_name}', '${creator_email}')`,
+        (err, response) => {
+          console.log('this is error', err, response)
+          err 
+          ? res.status(500).send('Database Error')
+          : res.status(200).send({ title, description, funds_goal, funds_raised, images, creator_name, creator_email })
+        })
+      }
+    })
+})
+
+app.patch('/products/:id', (req, res) => {
+    const { id } = req.params;
+    const { funds_raised } = req.body
+    pool.query(`UPDATE products SET funds_raised =${funds_raised} WHERE id=${id} RETURNING *`,
+    (err, response) => {
+      console.log(err, response)
+      err 
+      ? res.status(500).send('Database Error')
+      : res.status(200).send('Funds_raised successfully updated')
+    })
+})
 
 app.delete('/products/:id', (req, res) => {
-  const { id } = req.params;
-
-  queries.removeProduct(id)
-  .then(count => {
-    if (count > 0) {
-      res.status(200).send({ message: "Successfully delelted" })
-    } else {
-      res.status(404).send({ message: 'Unable to locate product' })
-    }
-  })
-  .catch(error => {
-    res.status(500).send(error.message)
-  })
-});
-
-app.patch('/products/:id/:amount', (req, res) => {
-  const { id, amount } = req.params;
-
-  queries.updateFundsRaised(id, amount)
-  .then(product => {
-    res.status(200).send({ message: 'Product successfully updated'})
-  })
-  .catch(error => {
-    res.status(500).send({ message: 'Unable to update product'})
-  })
+    const { id } = req.params;
+    pool.query(`DELETE FROM products WHERE id = ${id}`, 
+    (err, response) => {
+      console.log(err, response)
+      err 
+      ? res.status(500).send('Database Error')
+      : res.status(200).send('Product successfully deleted')
+    })
 })
 
 app.locals.title = "Kickstart This API"
 
-app.listen(port, () => {
-    console.log(`${app.locals.title} is running on port ${port}.`)
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on port ${app.get('port')}.`)
 })
